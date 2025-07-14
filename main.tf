cbdf8636c0f326fea25f25d8eea2ea0f815138bb
abhs1999 <abhilashhs50@gmail.com>
2025-07-14 19:33:02 +0530

    Create main.tf
    creating main.tf file

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "web_server" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
